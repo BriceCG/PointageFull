c6fad27bba525138107f67ce10d08b9ad5464f0a
BriceCG <briceainarivonyraharijaona@gmail.com>
2020-07-16 18:16:22 +0300

    config .env

--- src/helpers/axiosBase/index.js
+++ src/helpers/axiosBase/index.js
@@ -1,5 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+import {} from 'dotenv';
+require('dotenv').config()
+const PORT = process.env.REACT_APP_BACKEND_PORT
+const HOST = process.env.REACT_APP_BACKEND_HOST
+
 // instace axios
 export const axiosBase = axios.create({
-  baseURL: 'http://localhost:4000/'
+  baseURL: `http://${HOST}:${PORT}/`
 })
