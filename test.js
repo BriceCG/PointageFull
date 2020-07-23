f816b5cf628c40c1bf1789a728e5d881bad78014
BriceCG <briceainarivonyraharijaona@gmail.com>
2020-07-23 22:53:31 +0300

    fix: signUp and token

--- test.js
+++
@@ -1,7 +0,0 @@
-const {removeBlankSpace,containSubstring} = require('./api/service/stringService')
-
-async function  main(){
-    console.log(removeBlankSpace('ds fdss'))
-    console.log(containSubstring('Brice','Bre'))
-}
-main()
